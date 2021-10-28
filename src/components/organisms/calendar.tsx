6cfc885876274477ce14340aa73ccae20f194e33
Snorre Magnus Davøen <post@snorre.io>
2021-10-28 20:00:04 +0200

    Improved mobile friendly week calendar view
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- src/components/organisms/calendar.tsx
+++ src/components/organisms/calendar.tsx
@@ -1,4 +1,6 @@
 import { Auth0ContextInterface, User } from "@auth0/auth0-react";
+import { faBullseye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Transition } from "@headlessui/react";
 import { DateTime } from "luxon";
 import React, { Fragment, useState } from "react";
@@ -13,7 +15,167 @@ interface Calendar {
   auth0: Auth0ContextInterface<User>;
 }
 
-const Target = ({
+const WeekTarget = ({
+  target,
+  auth0,
+}: {
+  target: TargetRecord;
+  auth0: Auth0ContextInterface<User>;
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  return (
+    <button
+      key={target.id}
+      role="button"
+      border="rounded"
+      text="xs gray-300 center"
+      outline="focus:none"
+      focus="animate-pulse"
+      w="full"
+      p="1"
+      m="b-1"
+      onClick={() => setIsEditing(true)}
+    >
+      <span display="<md:hidden">{target.duration.toFormat("hh:mm")}</span>
+      <span display="md:hidden">{target.duration.toFormat("hh")}</span>
+      <TargetEditor
+        auth0={auth0}
+        target={target}
+        isOpen={isEditing}
+        close={() => setIsEditing(false)}
+      />
+    </button>
+  );
+};
+
+
+const WeekInterval = ({
+  interval,
+  auth0,
+}: {
+  interval: IntervalRecord;
+  auth0: Auth0ContextInterface<User>;
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const minutesPastMidnight =
+    interval.interval.beginning
+      .diff(interval.interval.beginning.startOf("day"))
+      .as("minutes");
+  const length =
+    (interval.interval.end ?? DateTime.now())
+      .diff(interval.interval.beginning)
+      .as("minutes");
+  return (
+    <div
+      pos="absolute"
+      w="full"
+      p="x-2"
+      style={{
+        top: minutesPastMidnight,
+      }}
+      onClick={() => setIsEditing(true)}
+    >
+      <button
+        key={interval.interval.beginning.toISODate()}
+        display="flex"
+        align="items-start"
+        bg="green-900 focus:green-800"
+        focus="outline-transparent"
+        ring="1 transparent :focus:green-400"
+        border="rounded"
+        text="xs gray-300 focus:gray-200"
+        p="1"
+        m="b-1"
+        w="full min-full"
+        h="min-6"
+        style={{
+          height: `${length}px`,
+        }}
+      >
+       <span display="<md:hidden">
+       {interval.interval.beginning.toFormat("HH:mm")} -{" "}
+        {interval.interval.end.isValid
+          ? interval.interval.end.toFormat("HH:mm")
+          : "now"}
+       </span>
+        <IntervalEditor
+          auth0={auth0}
+          interval={interval}
+          isOpen={isEditing}
+          close={() => setIsEditing(false)}
+        />
+      </button>
+    </div>
+  );
+};
+
+export const WeekCalendar = ({ dates, date, auth0 }: Calendar) => {
+  const fallbackDates = daysOfWeek(date);
+  return (
+    <div
+      display="flex"
+      flex="col"
+      justify="items-end"
+      w="full"
+      overflow="y-auto"
+    >
+      <div
+        p="b-4"
+        w="min-full full"
+        display="grid"
+        justify="items-stretch"
+        grid="gap-1 cols-[4rem_1fr_1fr_1fr_1fr_1fr_1fr_1fr]"
+        text="gray-400"
+      >
+        <div text="center" display="<md:hidden">Target</div>
+        <div text="center" display="md:hidden"><FontAwesomeIcon icon={faBullseye} /></div>
+        {dates &&
+          dates.map(({ target }) =>
+            target ? <WeekTarget auth0={auth0} target={target} /> : <div />
+          )}
+        <div display="grid" grid="rows-24 gap-1">
+          {[
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+            19, 20, 21, 22, 23,
+          ].map((hour) => {
+            return (
+              <div
+                display="flex"
+                justify="center"
+                bg="dark-500"
+                text="gray-600"
+                h="full"
+                w="full"
+              >
+                <span>
+                  {DateTime.fromObject({ hour, minute: 0, second: 0 }).toFormat(
+                    "HH"
+                  )}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+        {dates &&
+          dates.map(({ date, target, intervals }, i) => (
+            <div display="grid" grid="rows-24 gap-1" h="[1440px]" pos="relative">
+              {intervals.map((interval) => (
+                <WeekInterval auth0={auth0} interval={interval} />
+              ))}
+              {[
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+                18, 19, 20, 21, 22, 23,
+              ].map((hour) => {
+                return <div bg="dark-500" h="full"></div>;
+              })}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+const MonthTarget = ({
   target,
   auth0,
 }: {
@@ -45,127 +207,6 @@ const Target = ({
   );
 };
 
-const WeekInterval = ({
-  interval,
-  auth0,
-}: {
-  interval: IntervalRecord;
-  auth0: Auth0ContextInterface<User>;
-}) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const minutesPastMidnight =
-    interval.interval.beginning
-      .diff(interval.interval.beginning.startOf("day"))
-      .as("minutes") / 2;
-  const length =
-    (interval.interval.end ?? DateTime.now())
-      .diff(interval.interval.beginning)
-      .as("minutes") / 2;
-  return (
-    <div
-      pos="absolute"
-      w="full"
-      p="x-2"
-      style={{
-        top: minutesPastMidnight,
-      }}
-      onClick={() => setIsEditing(true)}
-    >
-      <button
-        key={interval.interval.beginning.toISODate()}
-        display="flex"
-        align="items-start"
-        bg="green-900 focus:green-800"
-        focus="outline-transparent"
-        ring="1 transparent :focus:green-400"
-        border="rounded"
-        text="xs gray-300 center focus:gray-200"
-        p="1"
-        m="b-1"
-        w="full min-full"
-        h="min-6"
-        style={{
-          height: `${length}px`,
-        }}
-      >
-        {interval.interval.beginning.toFormat("HH:mm")} -{" "}
-        {interval.interval.end.isValid
-          ? interval.interval.end.toFormat("HH:mm")
-          : "now"}
-        <IntervalEditor
-          auth0={auth0}
-          interval={interval}
-          isOpen={isEditing}
-          close={() => setIsEditing(false)}
-        />
-      </button>
-    </div>
-  );
-};
-
-export const WeekCalendar = ({ dates, date, auth0 }: Calendar) => {
-  const fallbackDates = daysOfWeek(date);
-  return (
-    <div
-      display="flex"
-      flex="col"
-      justify="items-end"
-      w="full"
-      overflow="y-auto"
-    >
-      <div
-        w="min-full full"
-        display="grid"
-        justify="items-stretch"
-        grid="gap-1 cols-[4rem_auto_auto_auto_auto_auto_auto_auto]"
-        text="gray-400"
-      >
-        <div text="center">Target</div>
-        {dates &&
-          dates.map(({ target }) =>
-            target ? <Target auth0={auth0} target={target} /> : <div />
-          )}
-        <div display="grid" grid="rows-24 gap-1">
-          {[
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-            19, 20, 21, 22, 23,
-          ].map((hour) => {
-            return (
-              <div
-                display="flex"
-                justify="center"
-                bg="dark-500"
-                text="gray-600"
-                h="full"
-                w="full"
-              >
-                <span>
-                  {DateTime.fromObject({ hour, minute: 0, second: 0 }).toFormat(
-                    "HH:mm"
-                  )}
-                </span>
-              </div>
-            );
-          })}
-        </div>
-        {dates &&
-          dates.map(({ date, target, intervals }, i) => (
-            <div display="grid" grid="rows-24 gap-1" h="[720px]" pos="relative">
-              {intervals.map((interval) => (
-                <WeekInterval auth0={auth0} interval={interval} />
-              ))}
-              {[
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
-                18, 19, 20, 21, 22, 23,
-              ].map((hour) => {
-                return <div bg="dark-500" h="full"></div>;
-              })}
-            </div>
-          ))}
-      </div>
-    </div>
-  );
-};
 
 const MonthInterval = ({
   interval,
@@ -245,7 +286,7 @@ export const MonthCalendar = ({ dates, date, auth0 }: Calendar) => {
             >
               <div display="flex" flex="row" justify="between" p="b-2">
                 <span text="gray-500">{date.day}</span>
-                {target && <Target target={target} auth0={auth0} />}
+                {target && <MonthTarget target={target} auth0={auth0} />}
               </div>
 
               {intervals.map((interval) => (
--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -9,7 +9,7 @@ import { Select } from "../components/atoms/select";
 import { queryToSearchString, useQuery } from "../utils/router";
 import { date, defaulted, Infer, optional, type } from "superstruct";
 import { IsoDate, LuxonDateTime, StepSchema } from "../api/schema";
-import { current, next, previous } from "../utils/date";
+import { current, daysOfWeek, next, previous } from "../utils/date";
 import { SwrMutateProvider } from "../components/providers/swr-mutation-provider";
 
 const QuerySchema = type({
@@ -49,6 +49,7 @@ interface CalendarHeader {
 }
 
 const CalendarHeader = ({ query, navigate }: CalendarHeader) => {
+  const weekDays = daysOfWeek(DateTime.now());
   return (
     <div
       pos="sticky top-0"
@@ -131,19 +132,13 @@ const CalendarHeader = ({ query, navigate }: CalendarHeader) => {
           }}
         />
       </div>
-      {query.step === "week" && <div w="min-3xl" bg="dark-800" display="grid" grid="cols-[4rem_auto_auto_auto_auto_auto_auto_auto] col-span-7">
+      {query.step === "week" && <div w="min-full full" bg="dark-800" display="grid" grid="cols-[4rem_1fr_1fr_1fr_1fr_1fr_1fr_1fr] col-span-7">
         <div></div>
-        {[
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
-          "Sunday",
-        ].map((day) => (
-          <div key={day} text="center" p="b-2">
-            {day}
+        {weekDays.map((day) => (
+          <div key={day.weekdayShort} text="center" p="b-2">
+            <span display="<md:hidden">{day.weekdayLong}</span>
+            <span display="<sm:hidden">{day.weekdayShort}</span>
+            <span display="sm:hidden" text="xs">{day.toFormat("EEEEE")}</span>
           </div>
         ))}
       </div>}
