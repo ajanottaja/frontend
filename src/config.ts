1fbb607ff9893100e68251047fbb93641271fd99
Snorre Magnus Davøen <post@snorre.io>
2025-02-01 21:52:50 +0100

    chore: Use bun and upgrade to latest dev dependencies
    Use bun to handle dependencies instead of npm. Bun is quite mature now
    and is really fast for installing dependencies.
    
    Upgrade to latest Vite and assorted other dev dependencies as the
    project had fallen quite behind on major versions. Seems everything
    still works without changing code.
    
    Remove old Auth0-related and API url config. This was a holdover from
    before moving to Supabase and should have been removed long ago.

--- src/config.ts
+++ src/config.ts
@@ -1,27 +1,14 @@
 import {
-  coerce,
-  define,
-  literal,
   string,
   type,
-  nullable,
-  enums,
-  Infer,
-  Struct,
-  unknown,
   create,
 } from "superstruct";
-// Gather relevant config in this file to make it easy to grok
 
-export const auth0 = {
-  domain: import.meta.env.VITE_AUTH0_DOMAIN,
-  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
-  redirectUri: window.location.origin,
-  audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-};
 
-// The host domain, e.g. api.ajanottaja.app
-export const apiHost = import.meta.env.VITE_API_URL;
+// The host domain, e.g. https://kncttlsgfupntuqgohgg.supabase.co
+
+console.log("import.meta.env.VITE_SUPABASE_URL", import.meta.env.VITE_SUPABASE_URL);
+console.log("import.meta.env.VITE_SUPABASE_ANON_KEY", import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export const supabaseConfig = create(
   {
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  VITE_AUTH0_DOMAIN: string;
-  VITE_AUTH0_CLIENT_ID: string;
-  VITE_AUTH0_AUDIENCE: string;
-  VITE_API_URL: string;
+  VITE_SUPABASE_URL: string;
+  VITE_SUPABASE_ANON_KEY: string;
 }
